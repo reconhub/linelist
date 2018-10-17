06f985d097e60482d08d574be257f7ef5c61e276
Thibaut Jombart <thibautjombart@gmail.com>
2018-10-17 11:31:13 +0100

    trying to use markdown throughout doc

--- R/clean_variable_names.R
+++ R/clean_variable_names.R
@@ -1,21 +1,21 @@
 #' Clean variable names
 #'
 #' This function standardises the variable names in a
-#' \code{data.frame}. It uses the standardisation implemented by
-#' \code{\link[epitrix]{clean_labels}} in the \code{epitrix} package. See
-#' \code{?epitrix::clean_labels} for more information.
+#' `data.frame`. It uses the standardisation implemented by
+#' [epitrix::clean_labels()] in the `epitrix` package. See
+#' `?epitrix::clean_labels` for more information.
 #'
 #' @author Thibaut Jombart
 #'
-#' @param x a \code{data.frame}
+#' @param x a `data.frame`
 #'
-#' @param ... further arguments passed to \code{epitrix::clean_labels}; the most
-#'   important is \code{sep}, which refers to the separator used between words,
-#'   and defaults to the underscore \code{_}.
+#' @param ... further arguments passed to [epitrix::clean_labels()]; the most
+#'   important is `sep`, which refers to the separator used between words,
+#'   and defaults to the underscore `_`.
 #'
 #' @export
 #'
-#' @return A \code{data.frame} with standardised variable names.
+#' @return A `data.frame` with standardised variable names.
 #'
 #' @examples
 #'
