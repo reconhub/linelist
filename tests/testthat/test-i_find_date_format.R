d8839e5f40156564892ec88cc432ea35606197a4
Thibaut Jombart <thibautjombart@gmail.com>
2018-10-25 13:08:53 +0100

    tests for Ymd format

--- tests/testthat/test-i_find_date_format.R
+++ tests/testthat/test-i_find_date_format.R
@@ -1,16 +1,31 @@
 
 context("test i_find_date_format")
 
-## test_that("function handles NULL", {
-## })
+test_that("function handles NULL", {
+  expect_true(is.na(i_find_date_format(NULL)))
+})
 
 
 test_that("test Ymd formats", {
 
   x <- as.Date("2001-01-01")
   
-  expected_result <- "%Y-%m-%d"
+  expected_format <- "%Y-%m-%d"
+  names(expected_format) <- "format"
+  expected_string <- as.character(x)
+  names(expected_string) <- "date"
   
-  expect_equal(i_find_date_format(x), expected_result)
-  
+  expect_equal(i_find_date_format(x)["format"], expected_format)
+  expect_equal(i_find_date_format(x)["date"], expected_string)
+
+  expect_equal(i_find_date_format(x), i_find_date_format("2001/01/01"))
+  expect_equal(i_find_date_format(x), i_find_date_format("2001 01 01"))
+  expect_equal(i_find_date_format(x), i_find_date_format("2001_01_01"))
+  expect_equal(i_find_date_format(x), i_find_date_format("2001.01.01"))
+
+  expect_equal(i_find_date_format(x), i_find_date_format("this is 2001/01/01"))
+  expect_equal(i_find_date_format(x), i_find_date_format("2001 01 01 a date 02-02-02"))
+  expect_equal(i_find_date_format(x), i_find_date_format("mixing-2001-01_01 separators-_"))
+  expect_equal(i_find_date_format(x), i_find_date_format("c'est ca: 2001.01.01 //../."))
+
 })
