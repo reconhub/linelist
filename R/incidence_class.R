c53cf1245fe66c8006575fc76c1f55b11674f5d1
Zhian N. Kamvar <zkamvar@gmail.com>
2018-11-06 13:20:06 +0000

    add ideas for incidence class

---
+++ R/incidence_class.R
@@ -0,0 +1,12 @@
+# It should also have accessor functions for standard columns:
+#   
+#  - date of onset
+#  - date of report
+#  - date of x where x in {death, hospitalization, ...}
+#  - sex
+#  - age
+#  - age group
+#  - a function that determines which concepts are available
+#  - arbitrary query function based on HXL?
+
+# Convert a data frame to a linelist via as_linelist
