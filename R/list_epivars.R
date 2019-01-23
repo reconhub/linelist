e92283505e17c3359e5e6a217a5bd570c88bbe35
Thibaut Jombart <thibautjombart@gmail.com>
2019-01-23 16:48:37 +0000

    get_meta is now list_epivars, with a new argument epivars_only

--- R/list_epivars.R
+++ R/list_epivars.R
@@ -1,9 +1,11 @@
 #' Retrieve metadata from a linelist object
 #'
-#' @param dat a `linelist` object
+#' @param x a `linelist` object
 #' @param dictionary if `TRUE`, a column called "*epivar*" is appended to the
 #'   metadata table, indicating which standard epi variable columns correspond
 #'   to; of `FALSE`, the class of the column replaces it
+#' @param epivars_only a `logical` indicating if columns which are not registers
+#'   `epivars` should be removed from the output
 #' @return a `data.frame` representing metatdata
 #' @export
 #' @examples
@@ -16,17 +18,22 @@
 #'                  )
 #' get_meta(ll)
 #' get_meta(ll, dict = FALSE)
-get_meta <- function(dat, dictionary = TRUE) {
-  stopifnot(inherits(dat, "linelist"))
-  res <- attr(dat, "epivars")$meta
+
+list_epivars <- function(x, epivars_only = FALSE, dictionary = TRUE) {
+  stopifnot(inherits(x, "linelist"))
+  res <- attr(x, "epivars")$meta
   if (!dictionary) {
     return(res)
   }
-  dict <- stack(attr(dat, "epivars")$vars, stringsAsFactors = FALSE)
+  dict <- stack(attr(x, "epivars")$vars, stringsAsFactors = FALSE)
   names(dict) <- c("column", "epivar")
   dict$epivar <- as.character(dict$epivar)
   out <- merge(dict, res, all = TRUE, sort = FALSE, stringsAsFactors = FALSE, drop = FALSE)
-  out <- out[match(names(dat), out$column), ]
+  out <- out[match(names(x), out$column), ]
   rownames(out) <- NULL
+  if (epivars_only) {
+    to_keep <- !is.na(out$epivar)
+    out <- out[to_keep, ]
+  }
   out
 }
