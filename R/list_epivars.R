1687f7be71c72b7b6f9da8f11bdf778c3999d705
Thibaut Jombart <thibautjombart@gmail.com>
2019-01-23 17:09:16 +0000

    adding a simple argt to list_epivars

--- R/list_epivars.R
+++ R/list_epivars.R
@@ -1,12 +1,17 @@
 #' Retrieve metadata from a linelist object
 #'
 #' @param x a `linelist` object
+#' @param simple a logical indicating if a single vector of names of present
+#'   `epivars` should be returned (`TRUE`); otherwise the output will contain
+#'   more information as a `data.frame` (`FALSE`, default)
 #' @param dictionary if `TRUE`, a column called "*epivar*" is appended to the
 #'   metadata table, indicating which standard epi variable columns correspond
 #'   to; of `FALSE`, the class of the column replaces it
 #' @param epivars_only a `logical` indicating if columns which are not registers
 #'   `epivars` should be removed from the output
-#' @return a `data.frame` representing metatdata
+#' @return if `simple` is `TRUE`, a vector of characters giving the names of
+#'   available `epivariables`; otherwise, a `data.frame` giving more information
+#'   about these variables
 #' @export
 #' @examples
 #' dat <- clean_data(messy_data())
@@ -18,18 +23,23 @@
 #'                  )
 #' list_epivars(ll)
 #' list_epivars(ll, TRUE)
-#' list_epivars(ll, dictrionary = FALSE)
+#' list_epivars(ll, epivars_only = TRUE)
+#' list_epivars(ll, dictionary = FALSE)
 
-list_epivars <- function(x, epivars_only = FALSE, dictionary = TRUE) {
+list_epivars <- function(x, simple = FALSE, epivars_only = FALSE, dictionary = TRUE) {
   stopifnot(inherits(x, "linelist"))
-  res <- attr(x, "epivars")$meta
+  if (simple) {
+    out <- names(attr(x, "epivars")$vars)
+    return(out)
+  }
+  out <- attr(x, "epivars")$meta
   if (!dictionary) {
-    return(res)
+    return(out)
   }
   dict <- stack(attr(x, "epivars")$vars, stringsAsFactors = FALSE)
   names(dict) <- c("column", "epivar")
   dict$epivar <- as.character(dict$epivar)
-  out <- merge(dict, res, all = TRUE, sort = FALSE, stringsAsFactors = FALSE, drop = FALSE)
+  out <- merge(dict, out, all = TRUE, sort = FALSE, stringsAsFactors = FALSE, drop = FALSE)
   out <- out[match(names(x), out$column), ]
   rownames(out) <- NULL
   if (epivars_only) {
