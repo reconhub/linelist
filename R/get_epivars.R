e11319633458901673faed5c7fbdfd020809c647
Zhian N. Kamvar <zkamvar@gmail.com>
2019-01-24 15:05:01 +0900

    add validation to get_epivars
    get_epivars now checks:
    
     - object has an epivars attribute
     - all vars are characters
     - all vars are in the global dictionary
     - all vars are present in the object
    
    Moreover, this will simplify the geo columns to
    a matrix as well.

--- R/get_epivars.R
+++ R/get_epivars.R
@@ -7,11 +7,13 @@
 #' @param ... epi variables to be used; can be provided as characters, integers,
 #'   or logical; integers and logicals are to be matched against the vector of
 #'   epi variables returned by `list_epivars(x, simple = TRUE)`.
-#' @param simplify if `TRUE` (default) and if the result is a single column,
-#'   then this variable will be returned as a `vector`; otherwise (`FALSE`), a
-#'   `data.frame` is returned.
+#' @param simplify if `TRUE` (default) and a single epivar is specified, it will
+#'   be converted to a `vector`. Otherwise, a data frame is returned.
 #' @return a vector if the epivar can be represented by a single column, a data
 #'   frame otherwise (e.g. a location column).
+#' @note For the "geo" epivar, which necessarily contains two columns for
+#'   longitude and latitude data, when `simplify = TRUE`, a matrix will be
+#'   returned.
 #' @export
 #' @rdname accessors
 #' @examples
@@ -27,30 +29,53 @@
 #'
 #' ## general purpose accessor
 #' list_epivars(ll, simple = TRUE)
-#' get_epivars(ll) # no epi variable
-#' get_epivars(ll, 1:3) # first 3 epi variables
+#' get_epivars(ll) # all epi variable
 #' get_epivars(ll, "id", "date_onset", "gender") # named epi variables
-#' get_epivars(ll, TRUE) # all epi variables
 #' date_of_onset(ll)
 #' id(ll)
 #' gender(ll)
-#' geo(ll)
-#' get_epivars(ll, "geo", vector = FALSE)
+#' geo(ll) # a matrix with two columns
+#' get_epivars(ll, "geo", simplify = FALSE) # a data frame with two columns
+#'
 #' # epivars that haven't been defined for the data set will return an error
 #' try(date_report(ll))
-
-
+#' @importFrom stats setNames
 #' @rdname accessors
 #' @export
 get_epivars <- function(x, ..., simplify = TRUE) {
-  ## TODO: this is lacking a validation step, which should throw informative
-  ## errors if ... are not valid epivars subsets
-  vars <- unlist(list(...))
+  if (is.null(attr(x, "epivars"))) {
+    stop("This object has no 'epivars' attribute")
+  }
+  vars <- list(...)
+  # No variables were defined, so return the subset of the data frame with the
+  # defined epivars
+  if (length(vars) == 0) {
+    return(as.data.frame(x)[unlist(attr(x, "epivars")$vars, use.names = FALSE)])
+  }
+  # A vector of variables was passed: turn it into a list
+  if (length(vars) == 1 && is.character(vars[[1]])) {
+    vars <- as.list(vars[[1]])
+  }
+  if (any(lengths(vars) > 1)) {
+    stop("All variables must be of length 1.")
+  }
+  are_characters <- vapply(vars, is.character, logical(1))
+  if (!all(are_characters)) {
+    stop("all variables must be characters.")
+  }
+  # validate the list
+  vars <- valid_dots(setNames(vars, unlist(vars, use.names = FALSE)))
   epivars <- attr(x, "epivars")$vars
-  to_keep <- unlist(epivars[vars])
+  valid_vars <- names(vars) %in% names(epivars)
+  if (!all(valid_vars)) {
+    invalids <- paste(names(vars)[!valid_vars], collapse = ", ")
+    stop(sprintf("The following epivars were not found in the data:\n%s", 
+                 invalids))
+  }
+  to_keep <- unlist(epivars[names(vars)], use.names = FALSE)
   out <- as.data.frame(x[to_keep])
-  if (simplify && length(to_keep) == 1L) {
-    out <- out[, 1, drop = TRUE]
+  if (simplify && length(vars) == 1L) {
+    out <- drop(as.matrix(out))
   }
   out
 }
