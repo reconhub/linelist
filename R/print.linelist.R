a2b34c1745301186b3b2e1347a63247c08f9d31d
Thibaut Jombart <thibautjombart@gmail.com>
2019-01-25 13:45:30 +0000

    adding print method

---
+++ R/print.linelist.R
@@ -0,0 +1,33 @@
+#' Function for printing linelist objects
+#'
+#' The `print` method of `linelist` objects shows *epivars* below the names of
+#' the variables.
+#'
+#' @export
+#'
+#' @param x a `linelist` object
+#'
+#' @param ... further arguments passed to `print.data.frame`
+#'
+#' @param show_epivars a logical indicating whether `epivars` should be shown
+#'   after the `data.frame` (`TRUE`, default), or not (`FALSE`); note that
+#'   `epivars` are only shown if present
+#'
+#' @examples
+#' x <- messy_data() %>%  clean_data() %>%
+#'   as_linelist(id = "id", gender = "gender", geo = c("lon", "lat"))
+#' x
+#' print(x, show_epivars = FALSE)
+
+print.linelist <- function(x, ..., show_epivars = TRUE) {
+  cat("<linelist object>\n\n")
+  epivars <- list_epivars(x)
+  print(as.data.frame(x), ...)
+
+  if (!is.null(epivars) && show_epivars) {
+    cat("\n // epivars:\n")
+    print(epivars)
+  }
+
+  return(invisible(NULL))
+}
