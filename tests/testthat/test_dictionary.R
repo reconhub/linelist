28872a238e6b071aa674ec0d215e3a9c2a39a6be
Zhian N. Kamvar <zkamvar@gmail.com>
2019-01-29 16:29:37 +0900

    rearranged tests

--- tests/testthat/test_dictionary.R
+++ tests/testthat/test_dictionary.R
@@ -1,6 +1,7 @@
 context("dictionary tests")
 
 reset_dictionary()
+
 hosp <- data.frame(
   epivar = "date_hospital",
   hxl    = "#date +start",
@@ -17,10 +18,13 @@ gdf <- data.frame(
 
 # The tests ---------------------------------------------------------
 
+# Tests for cromulence ----------------------------------------------
+
 test_that("epivars will return the default epivars", {
   expect_identical(get_dictionary(), getOption("linelist_dictionary"))
   expect_identical(get_dictionary(), default_dictionary())
 })
+
 
 
 test_that("a new epivar can be added", {
@@ -32,14 +36,7 @@ test_that("a new epivar can be added", {
                "The following epivars already exist in the dictionary:  date_hospital") 
 })
 
-# Errors and such ---------------------------------------------------
 
-test_that("check_dictionary() will thwart attempts to import incorrect epivars", {
-  expect_error(set_dictionary(pi), "is not a data frame but a numeric")
-  expect_error(set_dictionary("non/existant/file.txt"), "The file 'non/existant/file.txt' does not appear to exist")
-  expect_error(set_dictionary(iris), "x does not have 3 columns but 5")
-  expect_error(set_dictionary(iris[1:3]), "dictionary does not have the expected column names")
-})
 
 test_that("attempts to add existing epivars will be thwarted", {
   # works with character input
@@ -54,30 +51,57 @@ test_that("attempts to add existing epivars will be thwarted", {
                "The following epivars already exist in the dictionary:  geo, date_hospital")
 })
 
+
+
 test_that("a description can be updated", {
   add_description("date_hospital", "what")
   dict <- get_dictionary()
   expect_identical(dict$description[dict$epivar == "date_hospital"], "what")
 })
 
+
+
+test_that("set_epivars() will allow different column names", {
+  names(gdf) <- c("this", "is", "it")
+  gdf <- gdf[c(2, 1, 3)]
+  set_dictionary(gdf, epivar = "this", hxl = "is", description = "it")
+  gdf <- gdf[c(2, 1, 3)]
+  names(gdf) <- names(default_dictionary())
+  expect_identical(get_dictionary(), gdf)
+})
+
+
+
+test_that("check_dictionary() will thwart attempts to import incorrect epivars", {
+  expect_error(set_dictionary(pi),
+               "is not a data frame but a numeric")
+  expect_error(set_dictionary("non/existant/file.txt"), 
+               "The file 'non/existant/file.txt' does not appear to exist")
+  expect_error(set_dictionary(iris), 
+               "x does not have 3 columns but 5")
+  expect_error(set_dictionary(iris[1:3]), 
+               "dictionary does not have the expected column names")
+})
+
+
+
 test_that("there can be no description for a non-existant epivar", {
   expect_error(add_description("what", "the hell"),
                "add_definition\\(epivar = \\\"what\\\", description = \\\"the hell\\\", hxl = \\\"\\\"\\)")
 })
+
+
+# Resetting -----------------------------------------------------------
 
 test_that("set_dictionary() can take file input", {
   set_dictionary(system.file("example_dict.xlsx", package = "linelist"))
   expect_identical(get_dictionary(), rbind(default_dictionary(), gdf))
 })
 
-test_that("set_epivars() will allow different column names", {
-  names(gdf) <- c("this", "is", "it")
-  set_dictionary(gdf, epivar = "this", hxl = "is", description = "it")
-  names(gdf) <- names(default_dictionary())
-  expect_identical(get_dictionary(), gdf)
-})
+
 
 test_that("epivars can be reset", {
   reset_dictionary()
   expect_identical(default_dictionary(), get_dictionary())
 })
+
