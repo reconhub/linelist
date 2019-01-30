957109f190005f94684c9b4fdfb0f1a622a21567
Zhian N. Kamvar <zkamvar@gmail.com>
2019-01-30 14:48:50 +0900

    update lookup to return symbols by default; tests

--- R/lookup.R
+++ R/lookup.R
@@ -6,22 +6,67 @@
 #'
 #' @param x a [linelist][as_linelist] object. 
 #' @param epivar the name of an epivar defined in the [dictionary][get_dictionary()].
-#' @return a character vector of the column name
+#' @param symbol when `TRUE`, the column name will be converted to a symbol
+#'   for use in non-standard evaulation
+#' @return a symbol or character vector of the column name
+#' @export
 #' @examples
 #' dat <- clean_data(messy_data())
 #' ll  <- as_linelist(dat,
 #'                    id = "id", 
 #'                    gender = "gender",
+#'                    case_definition = "epi_case_definition",
 #'                    date_onset = "date_of_onset", 
 #'                    geo = c("lon", "lat")
 #'                   )
+#' # Lookup a column name, and return a character
+#' lookup(ll, "gender", symbol = FALSE)
 #' 
-lookup <- function(x, epivar = NULL) {
-  stopifnot(inherits(x, "linelist"))
+#' # If there are two columns, and `symbol = TRUE`, they are returned as a list
+#' # of symbols.
+#' lookup(ll, "geo")
+#' 
+#' # Using dplyr ---------------------------------------------------
+#'
+#' if (require("dplyr")) { withAutoprint({
+#'
+#'   # Two ways to use lookup with dplyr ---------------------------
+#'   #
+#'   # -- 1. Lookup in the pipe using the . replacement
+#'   ll %>% 
+#'     group_by(!!lookup(., "gender")) %>%  
+#'     count(!!lookup(., "case_definition"))
+#'
+#'   # -- 2. define temporary variables to lookup
+#'   (CASEDEF <- lookup(ll, "case_definition")) 
+#'   (GEO <- lookup(ll, "geo", symbol = FALSE))
+#'   ll %>%
+#'     group_by(!!CASEDEF) %>%
+#'     summarise_at(GEO, mean) # note, summarise_at uses characters
+#'
+#'   # Note: the linelist class is removed by dplyr verbs ----------
+#'   #
+#'   ll %>% group_by(!!CASEDEF) %>% class()
+#'
+#'   # The epivars are retained, so you can use as_linelist() to refresh
+#'   # (this even works for summarized data)
+#'   ll %>% 
+#'     group_by(!!CASEDEF) %>%
+#'     as_linelist() %>%
+#'     class()
+#' })}
+lookup <- function(x, epivar = NULL, symbol = TRUE) {
   ev <- attr(x, "epivars")
+  if (is.null(ev)) {
+    stop("This object does not contain an epivars attribute.")
+  }
   if (is.null(epivar)) {
     return(ev)
   }
   stopifnot(is.character(epivar))
-  ev[[epivar]]
+  res <- ev[[epivar]]
+  if (symbol) {
+    res <- if (length(res) == 1) as.symbol(res) else lapply(res, as.symbol)
+  }
+  res
 }
