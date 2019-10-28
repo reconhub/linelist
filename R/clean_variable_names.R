4012871fe1b0ddb85ba551e81383716353155404
Zhian N. Kamvar <zkamvar@gmail.com>
2019-10-28 11:50:42 +0000

    add initial control for duplicate variable names
    This will address #100

--- R/clean_variable_names.R
+++ R/clean_variable_names.R
@@ -41,13 +41,63 @@
 #' clean_data
 
 clean_variable_names <- function(x, protect = FALSE, ...) {
-  variable_names <- colnames(x)
+
+  variable_names <- new_names <- colnames(x)
+
   if (is.null(variable_names)) {
     stop('x has no column names')
   }
 
-  protect <- i_logical_from_int(protect, variable_names)
-  colnames(x)[!protect] <- epitrix::clean_labels(variable_names[!protect], ...)
+  # converting only the unprotected varaibles
+  protect <- i_logical_from_int(protect, new_names)
+  new_names[!protect] <- epitrix::clean_labels(new_names[!protect], ...)
+
+  # checking the variables and ammending them with numbers if they end up being
+  # the same
+  .dots     <- list(...)
+  sep       <- if (is.null(.dots[["sep"]])) "_" else .dots[["sep"]]
+
+  # Step 1: rearrange the vars so that protected are first (but if nothing is
+  # protected, then we create dummy subset variables
+  # Step 2: make things unique
+  # Step 3: rearrange things in the correct order
+  # Step 3: check if anything has changed
+  # Step 4: apply the suffixes to the new vector 
+
+  if (length(protect) > 1) {
+    protect_first <- c(which(protect), which(!protect))
+    in_order      <- order(protect_first)
+  } else {
+    protect_first <- in_order <- TRUE
+  }
+
+  var_names <- make.unique(new_names[protect_first], sep = sep)
+  var_names <- var_names[in_order]
+
+  var_names[protect] <- new_names[protect]
+  
+  if (!identical(var_names, new_names)) {
+    # If we get here, then that means some of the data had to be corrected
+    pat <- paste0("[", sep, "]\\d+$") 
+    dupes <- var_names != new_names & grepl(pat, var_names, perl = TRUE)
+    suffix <- sub(paste0("^.+(", pat, ")"), "\\1", var_names[dupes], perl = TRUE)
+    new_names[dupes] <- paste0(new_names[dupes], suffix)
+
+    varn <- format(variable_names[dupes])
+    newn <- format(new_names[dupes])
+    renames <- paste(varn, newn, 
+                     sep = " -> ",
+                     collapse = "\n  ")
+    
+    wrn <- paste("Some variable names were duplicated after cleaning and had",
+                 "suffixes attached:\n\n ",
+                 renames)
+    warning(wrn, call. = FALSE)
+    
+  } 
+
+  colnames(x) <- new_names
+
   # preserving the original variable names in a comment
   names(variable_names) <- colnames(x)
   comment(x) <- c(comment(x), variable_names)
